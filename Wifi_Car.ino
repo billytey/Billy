94c59d1727726548944da0532107fdbc74bcb6e8
billytey <47948772+billytey@users.noreply.github.com>
2019-02-25 01:37:56 +0800

    Update Wifi_Car.ino

--- Wifi_Car.ino
+++ Wifi_Car.ino
@@ -28,7 +28,7 @@ int dutyCycle = 512;    //0 - 1023
 
 void setup()
 {
-   pinMode(leftMotorForward, OUTPUT);
+  pinMode(leftMotorForward, OUTPUT);
   pinMode(rightMotorForward, OUTPUT); 
   pinMode(leftMotorBackward, OUTPUT);  
   pinMode(rightMotorBackward, OUTPUT);
